1852137899ca0fcf6ed638e510023872fd8be61a
wswall <wswallace510@gmail.com>
2024-06-20 13:20:52 +0200

    add iota and action history

--- micro/game_theory/extensive.py
+++ micro/game_theory/extensive.py
@@ -9,26 +9,22 @@ import networkx as nx
 os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"
 
 
-def _get_node_history(node):
+def _get_node_history(tree, node):
     history = []
     if node.parent_node is not None:
-        history.append(node.parent_node)
-        history.extend(_get_node_history(node.parent_node))
+        history.append(tree.get_action(node))
+        history.extend(_get_node_history(tree ,node.parent_node))
     return history
 
 
 class Node:
-    def __init__(self, player, parent_node=None, name=None, payout=None):
-        self.player = player
+    def __init__(self, parent_node=None, name=None, payout=None):
         self.parent_node = parent_node
-        self.name = name or id(self)
+        self.name = name if name is not None else id(self)
         self.payout = payout
 
     def update_payout(self, payout):
         self.payout = payout
-
-    def get_history(self):
-        return list(reversed(_get_node_history(self)))
 
     def is_terminal(self):
         return self.payout is not None
@@ -41,27 +37,39 @@ class Node:
 
 
 class ExtensiveGame:
-    def __init__(self, n_players, assign_id="len"):
-        self.players = list(range(n_players))
+    def __init__(self, assign_id="len"):
+        self.iota = {}
         self.assign_id = assign_id
         self.tree = nx.Graph()
-        self.tree.add_node(0, data=Node(1, name="root"))
 
     @property
     def root_node(self):
         return self.tree.nodes[0]["data"]
 
+    def get_action(self, node):
+        return self.tree[node.name][node.parent_node.name]
+
+    def get_history(self, node):
+        history = list(reversed(_get_node_history(self, node)))
+        return [h['action'] for h in history]
+
     def node_count(self):
         return len(self.tree.nodes)
 
-    def _generate_node(self, player, parent_name, name=None, payout=None):
+    def _generate_node(self, parent_name, name=None, payout=None):
         parent_node = self.tree.nodes[parent_name]["data"]
         if name is None and self.assign_id == "len":
             name = self.node_count()
-        return Node(player, parent_node, name=name, payout=payout)
+        return Node(parent_node=parent_node, name=name, payout=payout)
+
+    def set_root(self, player, name='root'):
+        root_node = Node(name=name)
+        self.iota[root_node] = player
+        self.tree.add_node(0, data=root_node)
 
     def add_node(self, player, parent_name, action, name=None, payout=None):
-        new_node = self._generate_node(player, parent_name, name=name, payout=payout)
+        new_node = self._generate_node(parent_name, name=name, payout=payout)
+        self.iota[new_node] = player
         self.tree.add_node(new_node.name, data=new_node)
         self.tree.add_edge(parent_name, new_node.name, action=action)
         return new_node
@@ -82,7 +90,7 @@ class ExtensiveGame:
                 payout = data["data"].payout
                 labels[node] = f"\n\n{payout}"
             else:
-                labels[node] = str(data["data"].player)
+                labels[node] = str(self.iota[data['data']])
         return labels
 
     def _get_edge_labels(self):
@@ -105,7 +113,8 @@ class ExtensiveGame:
     def _update_parent_payouts(self, terminal_nodes):
         parent_payouts = self._get_parent_payouts(terminal_nodes)
         for parent, payouts in parent_payouts.items():
-            parent.update_payout(max(payouts, key=lambda x: x[parent.player - 1]))
+            player = self.iota[parent] - 1
+            parent.update_payout(max(payouts, key=lambda x: x[player]))
 
     def backwards_induction_solve(self):
         extensive_game = deepcopy(self)
